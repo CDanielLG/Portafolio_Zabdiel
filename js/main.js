f8511d89f75b681ebeefa8c97f35951573777cc4
CDanielLG <hikivel139@gmail.com>
2022-08-01 17:02:32 -0500

    Se suben los nuevos archivos del portafolio, actualizaciones con bootstrap en html y css

---
+++ js/main.js
@@ -0,0 +1,10 @@
+alert("¡Gracias por visitar mi portafolio!");
+nombre=prompt("¿Cómo te llamas?");
+console.log("Su nombre es "+nombre);
+
+alert("Hola "+nombre);
+edad=prompt("¿Cuantos años tienes?");
+console.log("Su edad es: "+edad);
+MotivoVisita=prompt("¿Cual es el motivo de su visita?");
+console.log(MotivoVisita);
+console.log("Excelente, ojala sea de su agrado");
